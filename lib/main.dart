df9dcf6088708af56680bb18d40e5b23dd0c38b0
sylar0012 <kmc42692@naver.com>
2022-12-02 12:18:02 +0900

    좋아요 버튼

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:like_button/like_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +11,59 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    double buttonSize = 36;
+    return Scaffold(
+      body: Column(
+        children: [
+          LikeButton(
+            size: buttonSize,
+            circleColor: CircleColor(start: Colors.orangeAccent, end: Colors.orange),
+            bubblesColor: BubblesColor(
+              dotPrimaryColor: Colors.pink,
+              dotSecondaryColor: Colors.orange,
+            ),
+            likeBuilder: (bool isLiked) {
+              return Icon(
+                CupertinoIcons.heart_fill,
+                color: isLiked ? Colors.redAccent : Colors.grey,
+                size: buttonSize,
+              );
+            },
+            likeCount: 665,
+            countBuilder: (count, isLiked, text) {
+              var color = isLiked ? Colors.redAccent : Colors.grey;
+              Widget result;
+              if (count == 0) {
+                result = Text(
+                  "love",
+                  style: TextStyle(color: color),
+                );
+              } else
+                result = Text(
+                  text,
+                  style: TextStyle(color: color),
+                );
+              return result;
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
