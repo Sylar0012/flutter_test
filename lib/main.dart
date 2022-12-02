8287d878d6edd230552e370aa617c239836084e6
sylar0012 <kmc42692@naver.com>
2022-12-02 10:32:30 +0900

    그리드뷰 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,173 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.blueAccent),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverList(
+                delegate: SliverChildListDelegate([
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+              Text("OverFlow Test"),
+            ]))
+          ];
+        },
+        body: NSViewTest(),
+      ),
+    );
+  }
+}
+
+class NSViewTest extends StatefulWidget {
+  const NSViewTest({Key? key}) : super(key: key);
+
+  @override
+  State<NSViewTest> createState() => _NSViewTestState();
+}
+
+class _NSViewTestState extends State<NSViewTest> with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _buildTabBar(),
+        Expanded(
+          child: TabBarView(
+            controller: _tabController,
+            children: [
+              ListView(
+                children: [
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                  Text("OverFlow Test"),
+                ],
+              ),
+              Container(color: Colors.green),
+              Container(color: Colors.yellow),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  TabBar _buildTabBar() {
+    return TabBar(
+      controller: _tabController,
+      tabs: [
+        Text("메뉴", style: TextStyle(color: Colors.black)),
+        Text("정보", style: TextStyle(color: Colors.black)),
+        Text("리뷰", style: TextStyle(color: Colors.black)),
+      ],
+    );
   }
 }
